d7bb796261f2a9d239d276895adae27b9e3a258d
Ashti Nematian <NematianA@safarayaneh.local>
2023-09-27 15:14:19 +0330

    create new app that nemed account for register and login

---
+++ accouts/models.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+)
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils.translation import gettext as _
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+
+        if email is None:
+            raise TypeError('Users should have a Email')
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None):
+        if password is None:
+            raise ValueError('Password should not be none')
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+AUTH_PROVIDERS = {'google': 'google', 'email': 'email'}
+
+
+class Users(AbstractBaseUser, PermissionsMixin):
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='custom_user_permissions',
+        help_text=_('Specific permissions for this user.'),
+    )
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_author = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_adviser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    auth_provider = models.CharField(
+        max_length=255, blank=False,
+        null=False, default=AUTH_PROVIDERS.get('email'))
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+
